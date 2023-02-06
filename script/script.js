12ec9e37a95fa5b592fb4da7592b85ff913237e7
abduzar-coding <abduzar.web@gmail.com>
2023-02-06 21:43:45 +0000

    Updated nav bar

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function myFun() {
+    let x = document.getElementById('menuON')
+    if (x.className === 'menuON') {
+      x.className = 'menuOFF'
+		} else {
+			  x.className = 'menuON'
+			}
+}
